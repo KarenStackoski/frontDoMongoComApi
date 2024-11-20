009ed6437d42cca611159e3fe759c44e46845136
xLeotte <victorleottegaz@gmail.com>
2024-11-20 19:42:04 -0300

    Feito pagina de alunos
    Feita pagina de alunos conforme usuarios feito anteriormente

--- front-mongo-app/src/Menu/Menu.js
+++ front-mongo-app/src/Menu/Menu.js
@@ -1,19 +1,24 @@
-import './Menu.css'
+import './Menu.css';
 import { useNavigate } from 'react-router-dom';
 
 function Menu() {
     const navigate = useNavigate();
 
     const handleUser = (e) => {
-        e.preventDefault(); // Impede o comportamento padrão do formulário
-        navigate('/users'); // Navega para a página Menu
-      };
+        e.preventDefault(); // Impede o comportamento padrão do botão
+        navigate('/users'); // Navega para a página de Usuários
+    };
+
+    const handleStudent = (e) => {
+        e.preventDefault(); // Impede o comportamento padrão do botão
+        navigate('/students'); // Navega para a página de Alunos
+    };
 
     return (
         <div>
             <h1>Menu de opções</h1>
             <button className="options" onClick={handleUser}>Usuários</button>
-            <button className="options">Alunos</button>
+            <button className="options" onClick={handleStudent}>Alunos</button>
             <button className="options">Profissionais</button>
             <button className="options">Professores</button>
             <button className="options">Eventos</button>
--- front-mongo-app/src/index.js
+++ front-mongo-app/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Menu from './Menu/Menu';
-import Users from './Menu/Users/Users'
-import CreateUsers from './Menu/Users/CreateUser/Create'
+import Users from './Menu/Users/Users';
+import CreateUsers from './Menu/Users/CreateUser/Create';
+import Students from './Menu/Students/Students'; // Importando Students
+import CreateStudents from './Menu/Students/CreateStudents/Create'; // Importando CreateStudent
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,12 +16,15 @@ root.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/menu" element={<Menu />} />
-        <Route path='/users' element={<Users />}/>
-        <Route path='/createUsers' element={<CreateUsers />}/>
+        <Route path="/users" element={<Users />} />
+        <Route path="/createUsers" element={<CreateUsers />} />
+        <Route path="/students" element={<Students />} /> {/* Rota para lista de Alunos */}
+        <Route path="/createStudents" element={<CreateStudents />} /> {/* Rota para criar Alunos */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
