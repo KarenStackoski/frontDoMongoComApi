a64c7800f0181a399f7dfb73b1c6347efebc41e2
Mateus Martignago Mariot <mateusmartignagomariot@gmail.com>
2024-11-21 23:21:21 -0300

    Delete Funfando
    Ajuste no delete

--- front-mongo-app/src/Menu/Users/Users.js
+++ front-mongo-app/src/Menu/Users/Users.js
@@ -6,7 +6,13 @@ function Users() {
     const navigate = useNavigate();
 
     const [users, setUsers] = useState([]);
-    const [newUser, setNewUser] = useState('');
+    const [newUser, setNewUser] = useState({
+        userName: '',
+        userEmail: '',
+        userUser: '',
+        userLevel: '',
+        userStatus: false
+    });
 
     const getUsers = async () => {
         try {
@@ -15,76 +21,77 @@ function Users() {
         } catch (error) {
             console.log('Erro ao buscar todos os usuários', error);
         }
-    }
+    };
 
-    const getUsersByName = async () => {
+    const getUsersByName = async (userName) => {
         try {
             const response = await apiConfig.get(`/users/name/${userName}`);
             setUsers(response.data);
         } catch (error) {
             console.log('Erro ao buscar usuário por nome', error);
         }
-    }
+    };
 
-    const getUsersById = async () => {
+    const getUsersById = async (userId) => {
         try {
             const response = await apiConfig.get(`/users/${userId}`);
-            setUsers(response.data);
+            setUsers([response.data]);
         } catch (error) {
             console.log('Erro ao buscar usuário por id', error);
         }
-    }
+    };
 
     const postUsers = async () => {
         try {
-            const response = await apiConfig.post('/users', {
-                userName: userName,
-                userEmail: userEmail,
-                userUser: userUser,
-                userLevel: userLevel,
-                userStatus: userStatus
-            });
+            const response = await apiConfig.post('/users', newUser);
             setUsers([...users, response.data]);
-            setNewUser('');
+            setNewUser({
+                userName: '',
+                userEmail: '',
+                userUser: '',
+                userLevel: '',
+                userStatus: false
+            });
         } catch (error) {
             console.log('Erro ao criar usuário: ', error);
         }
-    }
+    };
 
-    //isso deve ser transferido pra página de editar
-    const putUsersById = async (idUser) => {
+    const putUsersById = async (idUser, updatedUser) => {
         try {
-            const response = await apiConfig.put(`/users/${userId}`, up);
+            const response = await apiConfig.put(`/users/${idUser}`, updatedUser);
+            setUsers(users.map(user => (user._id === idUser ? response.data : user)));
         } catch (error) {
             console.log('Não possível editar o usuário: ', error);
         }
-    }
+    };
 
-    const deleteUsersById = async () => {
+    const deleteUsersById = async (userId) => {
         try {
             await apiConfig.delete(`/users/${userId}`);
-            setUsers(users.filter((user) => user.userId !== userId));
+            setUsers(users.filter((user) => user._id !== userId));
         } catch (error) {
-            console.log('Não foi possível deletar o usuário: ', error);            
+            console.log('Não foi possível deletar o usuário: ', error);
         }
-    }
+    };
 
     useEffect(() => {
         getUsers();
     }, []);
 
     const handleCreateUser = (e) => {
-        e.preventDefault(); // Impede o comportamento padrão do formulário
-        navigate('/createUsers'); // Navega para a página Menu
-      };
+        e.preventDefault();
+        navigate('/createUsers');
+    };
 
     return (
         <div>
             <h1>Usuários</h1>
             <button onClick={handleCreateUser}>+ Criar</button>
-            <table border='2'>
+            <table border="2">
                 <thead>
                     <tr>
+                        <th>Id</th>
                         <th>Nome</th>
                         <th>E-mail</th>
                         <th>Usuário</th>
@@ -94,15 +101,16 @@ function Users() {
                     </tr>
                 </thead>
                 <tbody>
-                    {users.map(user => (
-                        <tr key={user.idUser}>
+                    {users.map((user) => (
+                        <tr key={user._id}>
+                            <td scope="row">{user._id}</td>
                             <td>{user.userName}</td>
                             <td>{user.userEmail}</td>
                             <td>{user.userUser}</td>
                             <td>{user.userLevel}</td>
                             <td>{user.userStatus ? 'Sim' : 'Não'}</td>
                             <td>
-                                <button onClick={() => deleteUsersById(user.userId)}>Deletar</button>
+                                <button onClick={() => deleteUsersById(user._id)}>Deletar</button>
                                 <button>Editar</button>
                             </td>
                         </tr>
