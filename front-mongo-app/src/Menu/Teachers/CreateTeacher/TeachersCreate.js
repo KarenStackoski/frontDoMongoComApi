6458f1933fc42a9d32cad8b5ca87159eff5f591a
Mateus Martignago Mariot <mateusmartignagomariot@gmail.com>
2024-11-23 11:09:31 -0300

    Finalização
    Finalização da criação, delete e edição dos professores

--- front-mongo-app/src/Menu/Teachers/CreateTeacher/TeachersCreate.js
+++ front-mongo-app/src/Menu/Teachers/CreateTeacher/TeachersCreate.js
@@ -1,29 +1,85 @@
 import './CreateTeachersd.css'
+import React, { useState, useEffect } from 'react';
+import apiConfig from '../../../api/apiConfig';
 
 function CreateTeachers() {
+    
+    const [teachers, setTeachers] = useState([]);
+    const [newTeacher, setNewTeacher] = useState({
+        teacherName: '',
+        teacherSchoolDisciplines: '',
+        teacherContact: '',
+        teacherPhone: '',
+        teacherStatus: false
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        setNewTeacher((prevTeacher) => ({
+            ...prevTeacher,
+            [name]: type === 'checkbox' ? checked : value
+        }));
+    };
+
+    const postTeacher = async () => {
+        try {
+            const response = await apiConfig.post('/teachers', newTeacher);
+            setTeachers([...teachers], response.data);
+            setNewTeacher({
+                teacherName: '',
+                teacherSchoolDisciplines: '',
+                teacherContact: '',
+                teachePhone: '',
+                teacherStatus: false
+            })
+        } catch (err) {
+            console.log(`Erro ao criar professor(a): ${err}`);
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        postTeacher();
+    };
+
     return(
         <div>
             <h1>Criando novo Professor</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="divInput">
                     <label>Nome</label>
-                    <input type="text"></input>
+                    <input type="text"
+                    name = 'teacherName'
+                    value = {newTeacher.teacherName}
+                    onChange={handleInputChange}></input>
                 </div>
                 <div className="divInput">
                     <label>Disciplina</label>
-                    <input type="text"></input>
+                    <input type="text"
+                    name = 'teacherSchoolDisciplines'
+                    value = {newTeacher.teacherSchoolDisciplines}
+                    onChange={handleInputChange}></input>
                 </div>
                 <div className="divInput">
                     <label>Telefone</label>
-                    <input type="text"></input>
+                    <input type="text"
+                    name = 'teacherPhone'
+                    value = {newTeacher.teacherPhone}
+                    onChange={handleInputChange}></input>
                 </div>
                 <div className="divInput">
                     <label>E-mail</label>
-                    <input type="select"></input>
+                    <input type="text"
+                    name = 'teacherContact'
+                    value = {newTeacher.teacherContact}
+                    onChange={handleInputChange}></input>
                 </div>
                 <div className="divInput">
                     <label>Status</label>
-                    <input type="checkBox"></input>
+                    <input type="checkBox"
+                    name = 'teacherStatus'
+                    value = {newTeacher.teacherStatus}
+                    onChange={handleInputChange}></input>
                 </div>
                 <button type='submit'>Salvar</button>
             </form>
--- front-mongo-app/src/Menu/Teachers/Teachers.js
+++ front-mongo-app/src/Menu/Teachers/Teachers.js
@@ -1,11 +1,86 @@
 import { useNavigate } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import apiConfig from '../../api/apiConfig';
 
 function Teachers() {
+
     const navigate = useNavigate();
 
+    const [teachers, setTeachers] = useState([]);
+    const [newTeacher, setNewTeacher] = useState({
+        teacherName: '',
+        teacherSchoolDisciplines: '',
+        teacherContact: '',
+        teacherPhone: '',
+        teacherStatus: false
+    });
+
+    const [editingTeacherId, setEditingTeacherId] = useState(null);
+
+    const getTeachers = async () => {
+        try {
+            const response = await apiConfig.get('/teachers');
+            console.log('usuários retornados', response.data);
+            setTeachers(response.data);
+        } catch (err) {
+            console.log('Erro ao buscar usuários: ', err);
+        }
+    }
+
+    const handleEditTeacher = (teacher) => {
+        setEditingTeacherId(teacher._id); 
+        setNewTeacher({
+            teacherName: teacher.teacherName,
+            teacherSchoolDisciplines: teacher.teacherSchoolDisciplines,
+            teacherContact: teacher.teacherContact,
+            teacherPhone: teacher.teacherPhone,
+            teacherStatus: teacher.teacherStatus,
+        });
+    };
+
+    const updateTeacher = async () => {
+        try {
+            const response = await apiConfig.put(`/teachers/${editingTeacherId}`, newTeacher);
+            setTeachers(
+                teachers.map((teacher) =>
+                    teacher._id === editingTeacherId ? response.data : teacher
+                )
+            );
+            setEditingTeacherId(null);
+            setNewTeacher({
+                teacherName: '',
+                teacherSchoolDisciplines: '',
+                teacherContact: '',
+                teacherPhone: '',
+                teacherStatus: false,
+            });
+        } catch (err) {
+            console.log(`Erro ao atualizar o professor id ${editingTeacherId}: ${err}`);
+        }
+    };
+
+    const deleteTeacherById = async (teacherId) =>{
+        try {
+            console.log (`Tentando deletar professor(a) id: ${teacherId}`);
+            await apiConfig.delete(`teachers/${teacherId}`);
+            setTeachers(teachers.filter((teacher)=> teacher._id !== teacherId))
+        } catch (err) {
+            console.log(`Não foi possível deletar o prof;essor(a) id ${teacherId}. Erro: ${err}`)
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        updateTeacher();
+    };
+
+    useEffect(() =>{
+        getTeachers();
+    }, []);
+
     const handleCreateTeachers = (e) => {
-        e.preventDefault(); // Impede o comportamento padrão do formulário
-        navigate('/createTeacher'); // Navega para a página Menu
+        e.preventDefault();  
+        navigate('/createTeacher'); 
       };
 
     return (
@@ -15,6 +90,7 @@ function Teachers() {
             <table border='2'>
                 <thead>
                     <tr>
+                        <th>Id</th>
                         <th>Nome</th>
                         <th>Discliplina</th>
                         <th>E-mail</th>
@@ -24,19 +100,92 @@ function Teachers() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>
-                            <button>Deletar</button>
-                            <button>Editar</button>
-                        </td>
-                    </tr>
+                    {teachers.map((teacher)=>(
+                        <tr key = {teacher._id}>
+                            <td>{teacher._id}</td>
+                            <td>{teacher.teacherName}</td>
+                            <td>{teacher.teacherSchoolDisciplines}</td>
+                            <td>{teacher.teacherContact}</td>
+                            <td>{teacher.teacherPhone}</td>
+                            <td>{teacher.teacherStatus ? 'Sim' : 'Não'}</td>
+                            <td>
+                                <button onClick={()=> deleteTeacherById(teacher._id)}>Deletar</button>
+                                <button onClick={() => handleEditTeacher(teacher)}>Editar</button>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
+
+            
+            {editingTeacherId && ( //A partir daqui ele cria os campos para edição do usuário
+                <div>
+                    <h2>Editando Professor</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <label>Nome:</label>
+                            <input
+                                type="text"
+                                name="teacherName"
+                                value={newTeacher.teacherName}
+                                onChange={(e) =>
+                                    setNewTeacher({ ...newTeacher, teacherName: e.target.value })
+                                }
+                            />
+                        </div>
+                        <div>
+                            <label>Disciplina:</label>
+                            <input
+                                type="text"
+                                name="teacherSchoolDisciplines"
+                                value={newTeacher.teacherSchoolDisciplines}
+                                onChange={(e) =>
+                                    setNewTeacher({
+                                        ...newTeacher,
+                                        teacherSchoolDisciplines: e.target.value,
+                                    })
+                                }
+                            />
+                        </div>
+                        <div>
+                            <label>E-mail:</label>
+                            <input
+                                type="text"
+                                name="teacherContact"
+                                value={newTeacher.teacherContact}
+                                onChange={(e) =>
+                                    setNewTeacher({ ...newTeacher, teacherContact: e.target.value })
+                                }
+                            />
+                        </div>
+                        <div>
+                            <label>Telefone:</label>
+                            <input
+                                type="text"
+                                name="teacherPhone"
+                                value={newTeacher.teacherPhone}
+                                onChange={(e) =>
+                                    setNewTeacher({ ...newTeacher, teacherPhone: e.target.value })
+                                }
+                            />
+                        </div>
+                        <div>
+                            <label>Status:</label>
+                            <input
+                                type="checkbox"
+                                name="teacherStatus"
+                                checked={newTeacher.teacherStatus}
+                                onChange={(e) =>
+                                    setNewTeacher({ ...newTeacher, teacherStatus: e.target.checked })
+                                }
+                            />
+                        </div>
+                        <button type="submit">Salvar Alterações</button>
+                        <button onClick={() => setEditingTeacherId(null)}>Cancelar</button>
+                    </form>
+                </div>
+            )}
+            
         </div>
     );
 }
